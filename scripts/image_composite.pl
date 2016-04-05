51fea9be25b977f5da522041dcc46431980ffd12
Andy Gallagher <andy.gallagher@theguardian.com>
2016-04-05 17:52:03 +0100

    bugfixes and logging tidyup

--- scripts/image_composite.pl
+++ scripts/image_composite.pl
@@ -43,6 +43,8 @@ my $output_key = "composite_image";
 if($ENV{'output_meta_key'}){
 	$output_key=$ENV{'output_meta_key'};
 }
+my $scale=$store->substitute_string($ENV{'output_scale'}) if($ENV{'output_scale'});
+
 $input_image=~s/"/\\"/; #escape quote marks so shell command can't be broken
 $temp_path=~s/"/\\"/;
 $overlay_image=~s/"/\\"/;
@@ -50,19 +52,20 @@ $output_image=~s/"/\\"/;
 
 print "INFO: Arguments:\n";
 print "Base image: $input_image\nOverlay image: $overlay_image\nOutput key: $output_key\nTemp path: $temp_path\n";
+print "Scale to: $scale\n\n" if($scale);
 
-die "-ERROR: Base image $base_image does not exist" if(! -f $base_image);
+die "-ERROR: Base image $input_image does not exist" if(! -f $input_image);
 die "-ERROR: Overlay image $overlay_image does not exist" if(! -f $overlay_image);
 die "-ERROR: Temp path $temp_path does not exist" if(! -d $temp_path);
 my $od=dirname($output_image);
 die "-ERROR: Output directory $od does not exist" if(! -d $od);
-print "-WARNING: Output image $output_image already exists and will be overwritten" if(-f $output_image);
+print "-WARNING: Output image $output_image already exists and will be overwritten\n" if(-f $output_image);
 
 my $processed_image;
 my $delete_processed=0;
 if($ENV{'output_scale'}){
 	print "INFO: Scaling base image to $scale...\n";
-	my $scale=$store->substitute_string($ENV{'output_scale'});
+	
 	if($scale !~ /^\d+x\d+$/){
 		die "-ERROR: $scale does not look like a correct scaling parameter. Should be digits, followed by 'x', followed by digits.\n";
 	}
@@ -97,7 +100,7 @@ unless($ENV{'no_set_media'}){
 	print FH "cf_media_file=$output_image\n";
 	close FH;
 }
-print "INFO: Deleting temp files";
+print "INFO: Deleting temp files\n";
 unlink($processed_image) if($delete_processed);
 print "+SUCCESS: $output_image output to meta:$output_key\n";
 
