9df2e6de1f0c70f4cc12d5cd9e3bdb18fbf3e841
Andy Gallagher <andy.gallagher@theguardian.com>
2016-04-07 16:32:34 +0100

    tidying up

--- Ruby/elementaltest.rb
+++ Ruby/elementaltest.rb
@@ -2,9 +2,8 @@
 $:.unshift(File.dirname(__FILE__)+'/lib')
 require 'Elemental/Job'
 require 'awesome_print'
-#user and passwd actually aren't used
-#api = ElementalAPI.new("10.235.51.110", user: ***REMOVED***, passwd: ***REMOVED***)
-api = ElementalAPI.new("10.235.51.110", login: "***REMOVED***", key: "***REMOVED***")
+
+api = ElementalAPI.new("10.235.51.110")
 ap api
 
 testfile = '/srv/Multimedia2/DAM/Media Libraries/Guardian UK Rushes/060_0815_01.mxf'
