859f1f841717ae001d541d3a6c0968d984657507
Andy Gallagher <andy.gallagher@theguardian.com>
2016-05-25 16:01:03 +0100

    adding route-name to standard substitutions

--- Ruby/lib/CDS/Datastore.rb
+++ Ruby/lib/CDS/Datastore.rb
@@ -448,6 +448,7 @@ min=sprintf("%02d",nowtime.min);
 sec=sprintf("%02d",nowtime.sec);
 
 begin
+rtn.gsub!('{route-name}',ENV['cf_routename'])
 rtn.gsub!('{year}',nowtime.year.to_s);
 rtn.gsub!('{month}',mon.to_s);
 rtn.gsub!('{day}',mday.to_s);
