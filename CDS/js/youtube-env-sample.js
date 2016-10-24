4a36b5b399c8ce92eeee874a782c45c386f69f9f
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-10-24 09:05:51 +0100

    add a sample env file for testing youtube upload without a route

---
+++ CDS/js/youtube-env-sample.js
@@ -0,0 +1,6 @@
+process.env['cnf_media_file'] = "";
+process.env['client_secrets'] = "";
+process.env['title'] = "";
+process.env['description'] = "";
+process.env['category_id'] = "";
+process.env['access'] = "";
