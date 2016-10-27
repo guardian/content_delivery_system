44ee8b2792aa09fe8b5228876a52d6ba35211062
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-10-27 12:11:38 +0100

    use service account for auth

--- CDS/js/test/youtube/TestYoutubeAuth.js
+++ CDS/js/test/youtube/TestYoutubeAuth.js
@@ -1,49 +1,188 @@
-const assert = require('assert');
-const sinon = require('sinon');
-const googleapis = require('googleapis');
-const OAuth2 = googleapis.auth.OAuth2;
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const Promise = require('promise');
+var chai = require('chai');
+var assert = chai.assert;
+var  chaiAsPromised = require('chai-as-promised');
+var sinon = require('sinon');
 
-const youtubeAuth = require('../../youtube/youtube-auth');
+chai.use(chaiAsPromised);
+
+var googleapis = require('googleapis');
+var OAuth2 = googleapis.auth.OAuth2;
+var AWS = require('aws-sdk');
+var Promise = require('promise');
+var pem = require('pem');
+var fs = require('fs');
+
+var youtubeAuth = require('../../youtube/youtube-auth');
 
 const CREDENTIALS_PATH = './test/youtube/lib/youtube_credentials.json';
+const INVALID_CREDENTIALS_PATH = './test/youtube/lib/invalid_youtube_credentials.json';
 
-describe('youtubeAuth', function() {
+describe('youtubeAuth', () => {
 
-    describe('getCredentials', function() {
+    describe('#readP12', () => {
 
+        it('should raise exception if private key or passphase is missing', () => {
 
-        it('should fetch credentials from bucket', function() {
+            return assert.isRejected(youtubeAuth.readP12(), 'Cannot upload to youtube: private key and passphrase required');
+        });
 
-           var getObject = AWS.S3.prototype.getObject = sinon.stub();
-           const buffer = new Buffer('"credentials"');
+        it('should save key in a file if private key and passphare are set', () => {
 
-           getObject.yields(null, {'Body': buffer});
+            process.env.private_key = 'key';
+            process.env.passphrase = 'passphrase';
 
-            return youtubeAuth.getCredentials()
-            .then(function(credentials) {
-                assert.equal(credentials, 'credentials');
+            var p12Read = pem.readPkcs12 = sinon.stub();
+            p12Read.yields(null, {key: 'key'});
+
+            var writeFile = fs.writeFileSync = sinon.stub();
+
+            return youtubeAuth.readP12()
+            .then(() => {
+                sinon.assert.calledOnce(p12Read);
+                sinon.assert.calledOnce(writeFile);
+                sinon.assert.calledWith(writeFile, './privatekey.pem', 'key', 'utf8');
             });
+
         });
     });
 
-    describe('getAuthClient', function() {
+    describe('#getCredentials', () => {
 
+        it('should raise an exception if no path to credentials file provided', () => {
 
-        it('should return auth client when credentials file is found', function(done) {
+            return assert.isRejected(youtubeAuth.getCredentials(), 'Cannot upload to youtube: client secrets file path not provided');
+        });
+
+        it('should raise an exception if credentials file cannot be parsed', () => {
+
+            process.env.client_secrets = INVALID_CREDENTIALS_PATH;
+            return assert.isRejected(youtubeAuth.getCredentials(), 'Cannot read credentials : SyntaxError: Unexpected token I in JSON at position 0');
+
+        });
+
+        it('should fetch credentials when path is provided', () => {
 
             process.env.client_secrets = CREDENTIALS_PATH;
-            const setCredentialsSpy = sinon.spy(OAuth2.prototype, 'setCredentials');
 
-            return youtubeAuth.getAuthClient().done(function(credentials) {
+            return assert.eventually.deepEqual(youtubeAuth.getCredentials(), { credentials: 'credentials'});
+        });
+    });
 
+    describe('getAuthClient', () => {
+
+        var p12Stub;
+
+        beforeEach(() => {
+            p12Stub = sinon.stub(youtubeAuth, 'readP12');
+        });
+
+        afterEach(() => {
+            p12Stub.restore();
+        });
+
+        it('should raise an error if client_id is missing', () => {
+            var credentialsStub = sinon.stub(youtubeAuth, 'getCredentials');
+            var promise = new Promise((fulfill, reject) => {
+                fulfill({
+                    client_secret: 'secret',
+                    web: {client_email: 'client_email' }
+                });
+            });
+
+            credentialsStub.returns(promise);
+
+
+            assert.isRejected(youtubeAuth.getAuthClient(), 'Credentials file is missing client_id property');
+
+            credentialsStub.restore();
+            return;
+
+        });
+
+        it('should raise an error if client_secret is missing', () => {
+            var credentialsStub = sinon.stub(youtubeAuth, 'getCredentials');
+            var promise = new Promise((fulfill, reject) => {
+                fulfill({
+                    client_id: 'id',
+                    web: {client_email: 'client_email' }
+                });
+            });
+
+            credentialsStub.returns(promise);
+
+            assert.isRejected(youtubeAuth.getAuthClient(), 'Credentials file is missing a client_secret property');
+            credentialsStub.restore();
+            return;
+
+        });
+
+        it('should raise an error if web property is missing', () => {
+            var credentialsStub = sinon.stub(youtubeAuth, 'getCredentials');
+            var promise = new Promise((fulfill, reject) => {
+                fulfill({
+                    client_id: 'id',
+                    client_secret: 'secret',
+                });
+            });
+
+            credentialsStub.returns(promise);
+
+            assert.isRejected(youtubeAuth.getAuthClient(), 'Credentials file is missing client_email property');
+            credentialsStub.restore();
+            return;
+
+        });
+
+        it('should raise an error if client_email is missing', () => {
+            var credentialsStub = sinon.stub(youtubeAuth, 'getCredentials');
+            var promise = new Promise((fulfill, reject) => {
+                fulfill({
+                    client_id: 'id',
+                    client_secret: 'secret',
+                    web: {}
+                });
+            });
+
+            credentialsStub.returns(promise);
+
+            assert.isRejected(youtubeAuth.getAuthClient(), 'Credentials file is missing client_email property');
+            credentialsStub.restore();
+            return;
+
+        });
+
+        it('should return auth client when credentials file is found', () => {
+
+            var credentialsStub = sinon.stub(youtubeAuth, 'getCredentials');
+            var promise = new Promise((fulfill, reject) => {
+                fulfill({
+                    client_id: 'id',
+                    client_secret: 'secret',
+                    web: {client_email: 'client_email' }
+                });
+            });
+
+            credentialsStub.returns(promise);
+
+            process.env.client_secrets = CREDENTIALS_PATH;
+            var setCredentialsSpy = sinon.spy(OAuth2.prototype, 'setCredentials');
+
+            var jwtStub = sinon.stub(googleapis.auth.JWT.prototype, 'authorize');
+            jwtStub.yields(null, {access_token: 'token'});
+
+
+            return youtubeAuth.getAuthClient()
+            .then((client) => {
+
+                assert(credentialsStub.calledOnce);
+                assert(p12Stub.calledOnce);
+                assert(jwtStub.calledOnce);
                 assert(setCredentialsSpy.calledOnce);
+                const authArg = setCredentialsSpy.lastCall.args[0];
+                assert.equal(authArg.access_token, 'token');
+                credentialsStub.restore();
+                return;
 
-                const authCredentials = setCredentialsSpy.lastCall.args[0];
-                assert.equal(authCredentials.refresh_token, 'refresh');
-                done();
             });
         });
 
--- CDS/js/youtube/youtube-auth.js
+++ CDS/js/youtube/youtube-auth.js
@@ -1,71 +1,97 @@
-const googleapis = require('googleapis');
-const OAuth2 = googleapis.auth.OAuth2;
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const Promise = require('promise');
-AWS.config.region = 'eu-west-1';
+var googleapis = require('googleapis');
+var OAuth2 = googleapis.auth.OAuth2;
+var fs = require('fs');
+var Promise = require('promise');
+var pem = require('pem');
+var Q = require('q');
 
-const CREDENTIALS_OBJECT_KEY = 's3-credentials.json';
-//If you are running this script locally and want to use a
-//local credentials file instead, comment out this line
-const CREDENTIALS_BUCKET = 'youtube-upload-credentials';
+const SECRET_KEY_FILE_PATH = './privatekey.pem';
+const SCOPES = [
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+    "https://www.googleapis.com/auth/youtube",
+    "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/youtube.upload"
+];
 
 function getCredentials() {
 
-    return new Promise(function(fulfill, reject) {
-        //const bucket = CREDENTIALS_BUCKET;
-        const filePath = process.env.client_secrets;
+    return new Promise((fulfill, reject) => {
 
-        if (!filePath && !bucket) {
-            reject (new Error('Cannot upload to youtube: no filepath or bucket name for credentials provided'));
+        const credentialsFile = process.env.client_secrets;
+        if (!credentialsFile) {
+            reject (new Error('Cannot upload to youtube: client secrets file path not provided'));
         }
 
-        if (bucket) {
-            const s3 = new AWS.S3();
-            return s3.getObject({
-                Bucket: bucket,
-                Key: CREDENTIALS_OBJECT_KEY
-            }, function(error, data) {
-                if (error) reject(error);
-                else {
-                    try {
-                        fulfill(JSON.parse(data.Body.toString()));
-                    } catch (ex) {
-                        reject(new Error('Invalid json in credentials bucket'));
-                    }
-                }
-            });
-        }
-
-        else if (filePath) {
-
-            try {
-                fulfill(JSON.parse(fs.readFileSync(filePath,'utf8')));
-            } catch (ex) {
-                reject(new Error('Cannot parse file ', filePath));
-            }
-
+        try {
+            fulfill(JSON.parse(fs.readFileSync(credentialsFile,'utf8')));
+        } catch (ex) {
+            reject(new Error('Cannot read credentials : ' + ex));
         }
     });
 }
 
+function readP12() {
+    return new Promise((fulfill, reject) => {
+
+        if (!process.env.private_key || !process.env.passphrase) {
+            reject (new Error('Cannot upload to youtube: private key and passphrase required'));
+        }
+
+        pem.readPkcs12(process.env.private_key, {p12Password: process.env.passphrase}, (err, result) => {
+            if (err) reject(err);
+            if (result) {
+                fs.writeFileSync(SECRET_KEY_FILE_PATH, result.key, 'utf8');
+                fulfill();
+            }
+        });
+    })
+}
 
 function getAuthClient() {
 
-    return getCredentials()
-    .then(function (credentials) {
+    return Q.all([this.getCredentials(), this.readP12()])
+    .spread((credentials) => {
 
-        var oauth2Client = new OAuth2(credentials.client_id, credentials.client_secret);
-        oauth2Client.setCredentials({
-            refresh_token: credentials.refresh_token
+        if (!credentials.client_id) {
+            throw new Error('Credentials file is missing client_id property');
+        }
+
+        if (!credentials.client_secret) {
+            throw new Error('Credentials file is missing a client_secret property');
+        }
+
+        if (!credentials.web || !credentials.web.client_email) {
+            throw new Error('Credentials file is missing a web.client_email property');
+        }
+
+        var oauth2 = new OAuth2(credentials.client_id, credentials.client_secret);
+
+        var jwt = new googleapis.auth.JWT(
+            credentials.web.client_email,
+            SECRET_KEY_FILE_PATH,
+            null,
+            SCOPES
+        );
+
+        return new Promise((fulfill, reject) => {
+
+            jwt.authorize((err, result) => {
+                if (err) err
+                else {
+                    oauth2.setCredentials({
+                        access_token: result.access_token
+                    });
+
+                    fulfill(oauth2);
+                }
+            });
         });
-
-        return oauth2Client;
     });
 }
 
 module.exports = {
     getAuthClient: getAuthClient,
-    getCredentials: getCredentials
+    getCredentials: getCredentials,
+    readP12: readP12
 };
 
