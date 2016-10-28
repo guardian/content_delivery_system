0b564c7eb79ee708fd282000c97dbc31ebe0b878
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-10-28 09:07:49 +0100

    add sample env

--- CDS/js/youtube/youtube-env-sample.js
+++ CDS/js/youtube/youtube-env-sample.js
@@ -1,9 +1,13 @@
 //Speficy either the name of the bucket from which
 //credentials are loaded or a local secrets file
 process.env['cnf_media_file'] = "";
-process.env['credentials_bucket'] = "";
 process.env['client_secrets'] = "";
+process.env['passphrase'] = "";
+process.env['private_key'] = "";
+process.env['cf_datastore_location'] = "";
 process.env['title'] = "";
 process.env['description'] = "";
 process.env['category_id'] = "";
 process.env['access'] = "";
+process.env['owner_account'] = "";
+process.env['owner_channel'] = "";
--- CDS/js/youtube/youtube-upload.js
+++ CDS/js/youtube/youtube-upload.js
@@ -3,11 +3,14 @@
 //This method uploads the video specified in the cnf_media_file parameter to youtube.
 //It expects the following arguments:
 //<cnf_media_file> - the path to the media file you are uploading
+//<client_secrets> - path to the client secrets file with client id, client secret and client email
+//<private_key> path to encrypted private key file
+//<passphrase> passphrase for the key file
 //<title> - the title of the video to be uploaded
 //<description> - the description of the video
 //<category_id> - the category id for the video
 //<access> - the level of access for the video
-//<channel> - [OPTIONAL] the channel you are uploading to
+//<owner_channel> - [OPTIONAL] the channel you are uploading to
 //<owner_account> - [OPTIONAL] The account that the video is uploaded on behalf of. You can ommit the channel and owner_account parameters, but this means you cannot upload videos of accounts with multiple channels. If you specify the channel to upload to, then you must also specify the owner account you are uploading on behalf of.
 
 const youtubeLib = require('./youtube-upload-lib');
