951389338f367cdd31e69f0fd543a61b51d77132
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-10-28 13:04:51 +0100

    add a sample script to upload to youtube locally

---
+++ CDS/js/youtube/youtube-upload-local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+cd "${0%/*}"
+data_store="/tmp/test.db"
+if [ -f $data_store ]; then
+    rm $data_store
+fi
+cd ../..
+export "cf_datastore_location"=$data_store;
+./cds_create_datastore.pl
+./cds_datastore.pl set meta key value
+export "cnf_media_file"="";
+export "client_secrets"="";
+export "title"="";
+export "description"="";
+export "category_id"="";
+export "access"="";
+export "owner_account"="";
+export "owner_channel"="";
+export "passphrase"="";
+export "private_key"="";
+cd js/youtube;
+node ./youtube-upload.js
