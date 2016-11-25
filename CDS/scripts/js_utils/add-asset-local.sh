711cc893af04f7b7829efd1618fa25d6adccebe1
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-11-25 10:48:49 +0000

    fix add asset

--- CDS/scripts/js_utils/add-asset-local.sh
+++ CDS/scripts/js_utils/add-asset-local.sh
@@ -5,7 +5,7 @@ if [ -f $data_store ]; then
     rm $data_store
 fi
 export "cf_datastore_location"=$data_store;
-cd ..
+cd ../..
 ./cds_create_datastore.pl
 #Change the values here to what you want to get set in the datastore
 ./cds_datastore.pl set meta youtube_id id
