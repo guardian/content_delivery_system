fa69b3599f10c16e4dece0bbf93f9eeba30551fd
Dave Allison <david.allison@theguardian.com>
2019-06-07 15:22:35 +0100

    Making indents consistent and removing references to files

--- CDS/scripts/png_to_jpeg.rb
+++ CDS/scripts/png_to_jpeg.rb
@@ -4,8 +4,7 @@
 #It depends on ImageMagick being available on the local node (apt-get install ImageMagick or yum install ImageMagick or port install ImageMagick etc.), specifically the 'convert' and 'identify' commands.
 #
 #Arguments:
-# <input_file>file1|/path/to/file2|{meta:FileNameKey} - use the provided image file
-# <output_path>/path/to/output/files - put the output files in this place
+# <input_key>{meta:FileNameKey} - use the provided key as an input
 # <output_key>keyname - output the path of the file into this key in the meta: section of the datastore
 
 #END DOC
@@ -15,7 +14,7 @@ require 'CDS/Datastore'
 #START MAIN
 store=Datastore.new('png_to_jpeg')
 
-input_image=store.substitute_string(ENV['input_file'])
+input_image=store.substitute_string(ENV['input_key'])
 
 cmdline="identify -format '%m' '#{input_image}'"
 
@@ -31,9 +30,8 @@ end
 output_path=store.substitute_string(ENV['output_path'])
 
 if(ENV['output_key'])
-    keyname=ENV['output_key']
-    store.set('meta',keyname,input_image)
-
+  keyname=ENV['output_key']
+  store.set('meta',keyname,input_image)
 else
-    puts "-WARNING: No <output_key> specified so not outputting image path to datastore."
+  puts "-WARNING: No <output_key> specified so not outputting image path to datastore."
 end
