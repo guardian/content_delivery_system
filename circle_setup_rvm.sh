05d80b077b2010e10bb9158f668d738242d86b5a
Andy Gallagher <andy.gallagher@theguardian.com>
2019-10-07 19:57:23 +0100

    dummy update

--- circle_setup_rvm.sh
+++ circle_setup_rvm.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+
 echo export PATH="$PATH:/opt/circleci/.rvm/bin" >> /root/.profile
 echo '[[ -s "/opt/circleci/.rvm/scripts/rvm" ]] && source "/opt/circleci/.rvm/scripts/rvm"' >> /root/.profile
