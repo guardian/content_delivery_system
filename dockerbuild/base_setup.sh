73ceeb92d09f18a33e966eebd273c27a8c527400
Andy Gallagher <andy.gallagher@theguardian.com>
2019-10-09 10:23:29 +0100

    install from package instead

--- dockerbuild/base_setup.sh
+++ dockerbuild/base_setup.sh
@@ -13,7 +13,7 @@ echo ------------------------------------------
 DEBIAN_FRONTEND=noninteractive apt-get -y install python-pip zip perl ffmpeg2theora libz-dev libcrypt-ssleay-perl liburi-encode-perl libnet-ssleay-perl libnet-idn-encode-perl \
 liblwp-protocol-https-perl libdbd-sqlite3-perl libyaml-perl libxml-sax-expatxs-perl libxml-xpath-perl libwww-perl libtemplate-perl  \
 libxml-simple-perl libjson-perl libjson-xs-perl libdate-manip-perl libnet-sslglue-perl libdigest-perl libdigest-sha-perl libdatetime-perl libdatetime-format-http-perl \
-libdbi-perl libhtml-stream-perl libfile-slurp-unicode-perl cpanminus zlib1g-dev build-essential s3cmd libsqlite3-dev nodejs curl
+libdbi-perl libhtml-stream-perl libfile-slurp-unicode-perl cpanminus zlib1g-dev build-essential s3cmd libsqlite3-dev nodejs curl libdbd-pg-perl
 pip --no-cache-dir install awscli
 
 rm -rf /var/cache/apt
