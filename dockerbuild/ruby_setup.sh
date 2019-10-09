13639856fcbc6c75a7f8a47ca3e5059ffad17ba9
Andy Gallagher <andy.gallagher@theguardian.com>
2019-10-09 17:58:35 +0100

    adding xmp and mail gems

--- dockerbuild/ruby_setup.sh
+++ dockerbuild/ruby_setup.sh
@@ -9,4 +9,4 @@ DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get -y update
 apt-get -y install ruby2.6 ruby2.6-dev ruby-switch
 ruby-switch --set ruby2.6
-gem install awesome_print trollop sentry-raven aws-sdk-v1 aws-sdk-core aws-sdk-resources google-api-client:'<0.9' launchy thin rest-client certifi sentry-raven elasticsearch
+gem install awesome_print trollop sentry-raven aws-sdk-v1 aws-sdk-core aws-sdk-resources google-api-client:'<0.9' launchy thin rest-client certifi sentry-raven xmp mail elasticsearch
