df23646e69ad11c744b9857e96de1ec33d8e74e5
Andy Gallagher <andy.gallagher@theguardian.com>
2019-10-09 10:05:52 +0100

    adding postgres driver

--- dockerbuild/perl_setup.sh
+++ dockerbuild/perl_setup.sh
@@ -9,4 +9,4 @@ curl -L https://cpanmin.us/ -o cpanm
 chmod +x cpanm
 apt-get -y install libdbd-mysql-perl
 ./cpanm --force Amazon::SQS::Simple
-./cpanm Data::UUID URL::Encode Net::FTP::Throttle Digest::SHA1 File::Touch Search::Elasticsearch Digest::HMAC_SHA1
+./cpanm Data::UUID URL::Encode Net::FTP::Throttle Digest::SHA1 File::Touch Search::Elasticsearch Digest::HMAC_SHA1 DBD::Pg
