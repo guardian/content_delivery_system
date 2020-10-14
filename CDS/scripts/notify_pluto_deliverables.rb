01516049b8ca14bdccfc287d6dde255d09f603f6
Andy Gallagher <andy.gallagher@theguardian.com>
2020-10-14 15:54:27 +0100

    temporarily disable ssl validation

--- CDS/scripts/notify_pluto_deliverables.rb
+++ CDS/scripts/notify_pluto_deliverables.rb
@@ -67,7 +67,7 @@ puts "DEBUG: uri is #{uri}" if $debug
 puts "DEBUG: request is #{json_body}" if $debug
 
 loop do
-  response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :debug_output=>$stdout) do |http|
+  response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :debug_output=>$stdout, :verify_mode=>OpenSSL::SSL::VERIFY_NONE) do |http|
     request = Net::HTTP::Post.new(uri, 'Content-Type'=>'application/json')
     request.basic_auth(user, passwd)
     request.body = json_body
