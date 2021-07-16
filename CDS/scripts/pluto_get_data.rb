13db219080e4493fd62b095889cb5cb8b1157ae8
Dave Allison <david.allison@theguardian.com>
2021-07-16 12:06:30 +0100

    Adding a new method

--- CDS/scripts/pluto_get_data.rb
+++ CDS/scripts/pluto_get_data.rb
@@ -12,46 +12,34 @@ require 'awesome_print'
 require 'net/http'
 require 'json'
 
+# Method to get JSON from a given API URL
+
+def get_json(input_url)
+  uri = URI(input_url)
+
+  Net::HTTP.start(uri.host, uri.port,
+    :use_ssl => uri.scheme == 'https',
+    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+    request = Net::HTTP::Get.new uri
+    $method_response = http.request request
+  end
+
+  return JSON.parse($method_response.body)
+end
+
 #START MAIN
 $store = Datastore.new('pluto_get_data')
 
 # Get the project data
 
-uri = URI('https://' + $store.substitute_string(ENV['server']) + '/pluto-core/api/project/' + $store.substitute_string(ENV['project_id']))
-
-Net::HTTP.start(uri.host, uri.port,
-  :use_ssl => uri.scheme == 'https',
-  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-  request = Net::HTTP::Get.new uri
-  $response = http.request request
-end
-
-returned_json = JSON.parse($response.body)
+returned_json = get_json('https://' + $store.substitute_string(ENV['server']) + '/pluto-core/api/project/' + $store.substitute_string(ENV['project_id']))
 
 # Get the commission data
 
-commission_uri = URI('https://' + $store.substitute_string(ENV['server']) + '/pluto-core/api/pluto/commission/' + returned_json["result"]["commissionId"].to_s)
-
-Net::HTTP.start(commission_uri.host, commission_uri.port,
-  :use_ssl => commission_uri.scheme == 'https',
-  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-  request = Net::HTTP::Get.new commission_uri
-  $commission_response = http.request request
-end
-
-commission_returned_json = JSON.parse($commission_response.body)
+commission_returned_json = get_json('https://' + $store.substitute_string(ENV['server']) + '/pluto-core/api/pluto/commission/' + returned_json["result"]["commissionId"].to_s)
 
 # Get the working group data
 
-group_uri = URI('https://' + $store.substitute_string(ENV['server']) + '/pluto-core/api/pluto/workinggroup/' + returned_json["result"]["workingGroupId"].to_s)
-
-Net::HTTP.start(group_uri.host, group_uri.port,
-  :use_ssl => group_uri.scheme == 'https',
-  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-  request = Net::HTTP::Get.new group_uri
-  $group_response = http.request request
-end
-
-group_returned_json = JSON.parse($group_response.body)
+group_returned_json = get_json('https://' + $store.substitute_string(ENV['server']) + '/pluto-core/api/pluto/workinggroup/' + returned_json["result"]["workingGroupId"].to_s)
 
 $store.set('meta',{ "commission" => commission_returned_json["result"]["title"], "workinggroup" => group_returned_json["result"]["name"]})
