d2c8a8ab22fa4f58e4a1e4ad8c6664826e34346c
Richard Pryce <richard.pryce@guardian.co.uk>
2023-08-21 12:11:22 +0100

    Pin Log::Any@1.716 to fix Elasticsearch build issue

--- dockerbuild/perl_setup.sh
+++ dockerbuild/perl_setup.sh
@@ -9,4 +9,5 @@ curl -L https://cpanmin.us/ -o cpanm
 chmod +x cpanm
 apt-get -y install libdbd-mysql-perl
 ./cpanm --force Amazon::SQS::Simple
+./cpanm Log::Any@1.716
 ./cpanm Data::UUID URL::Encode Net::FTP::Throttle Digest::SHA1 File::Touch Search::Elasticsearch Digest::HMAC_SHA1
