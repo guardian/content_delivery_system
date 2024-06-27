fc8f567b8890b80479fbb92d613c7c38870b2918
Richard Pryce <richard.pryce@guardian.co.uk>
2024-06-27 12:31:04 +0100

    Bump Ruby to 3.3

--- dockerbuild/ruby_setup.sh
+++ dockerbuild/ruby_setup.sh
@@ -7,9 +7,11 @@ echo ------------------------------------------
 DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common
 DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get -y update
-apt-get -y install ruby2.7 ruby2.7-dev ruby-switch
-ruby-switch --set ruby2.7
+apt-get -y install ruby3.3 ruby3.3-dev ruby-switch
+ruby-switch --set ruby3.3
 gem install nokogiri -v 1.13.10
-gem install retryable -v 3.0.5
+gem install faraday -v 2.8.1
+gem install faraday-net_http -v 3.0.2
+gem install retryable -v 3.1.0
 gem install awesome_print trollop sentry-raven aws-sdk-v1 aws-sdk-core aws-sdk-resources google-api-client:'<0.9' \
     launchy thin rest-client certifi sentry-raven xmp mail exifr elasticsearch rake rspec webmock
