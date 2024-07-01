9a215a87520af561644294958e4b602eb4df30dd
Richard Pryce <richard.pryce@guardian.co.uk>
2024-07-01 16:52:46 +0100

    Fix cds_setup script

--- dockerbuild/cds_setup.sh
+++ dockerbuild/cds_setup.sh
@@ -156,14 +156,14 @@ chmod ${LOG_PERM} ${LOG_PATH}
 mkdir -p ${BINARIES_PATH}
 #mkdir -p ${MAPPINGS_PATH}
 
-if [ ! -x $(which perl) ]; then
+if [ ! -x `which perl` ]; then
 	echo ERROR - you do not appear to have Perl installed, which is needed to run CDS.
 	echo Please install a recent version of Perl for your OS and re-run this script.
-	exit 1
+	exit 1;
 fi
 
 
-for x in $(ls "${SOURCE_DIR}/scripts"); do
+for x in `ls "${SOURCE_DIR}/scripts"`; do
 	if [ -f "${SOURCE_DIR}/scripts/$x" ]; then
 		install -bcpv -g ${OWNER_GID} -m ${MODULES_PERM} -o ${OWNER_UID} "${SOURCE_DIR}/scripts/$x" ${MODULES_PATH}
 		echo ${MODULES_PATH}/$x >> $MANIFEST
@@ -173,7 +173,7 @@ done
 echo
 echo
 
-for x in $(ls "${SOURCE_DIR}/test_routes"); do
+for x in `ls "${SOURCE_DIR}/test_routes"`; do
         if [ -f "${SOURCE_DIR}/test_routes/$x" ]; then
                 install -bcpv -g ${OWNER_GID} -m ${ROUTES_PERM} -o ${OWNER_UID} -p -v "${SOURCE_DIR}/test_routes/$x" ${ROUTES_PATH}
 		echo ${ROUTES_PATH}/$x >> $MANIFEST
@@ -183,7 +183,7 @@ done
 echo
 echo
 
-for x in $(ls "${SOURCE_DIR}/scripts/templates"); do
+for x in `ls "${SOURCE_DIR}/scripts/templates"`; do
         if [ -f "${SOURCE_DIR}/scripts/templates/$x" ]; then
                 install -bcpv -g ${OWNER_GID} -m ${TEMPLATES_PERM} -o ${OWNER_UID} "${SOURCE_DIR}/scripts/templates/$x" ${TEMPLATES_PATH}
 		echo ${TEMPLATES_PATH}/$x >> $MANIFEST
@@ -193,7 +193,83 @@ done
 echo
 echo
 
-#for x in $(ls "${SOURCE_DIR}/mappings"); do
+#for x in `ls "${SOURCE_DIR}/mappings"`; do
 #        if [ -f "${SOURCE_DIR}/mappings/$x" ]; then
 #                #install -bcpv -g ${OWNER_GID} -m ${MAPPINGS_PERM} -o ${OWNER_UID} "${SOURCE_DIR}/mappings/$x" ${MAPPINGS_PATH}
-#		echo ${MAPPINGS_PATH
+#		echo ${MAPPINGS_PATH}/$x >> $MANIFEST
+#        fi
+#done
+
+echo
+echo
+
+for x in `ls "${SOURCE_DIR}"/{cds_run.pl,cds_datareport.pl,resolve_name.pl,newsml_get.pl,ee_get.pl,cds_datastore.pl,saxnewsml.pm}`; do
+        if [ -f "$x" ]; then
+                install -bcpv -g ${OWNER_GID} -m ${BINARIES_PERM} -o ${OWNER_UID} "$x" ${BINARIES_PATH}
+		echo ${BINARIES_PATH}/`basename "$x"` >> $MANIFEST
+        fi
+done
+
+echo
+echo
+
+echo Creating path for in-process data ${SPOOL_PATH}...
+mkdir -p "${SPOOL_PATH}"
+chown ${OWNER_UID} "${SPOOL_PATH}"
+chgrp ${OWNER_GID} "${SPOOL_PATH}"
+chmod ${SPOOL_PERM} "${SPOOL_PATH}"
+
+echo Creating path for cache data ${CACHE_PATH}...
+mkdir -p "${CACHE_PATH}"
+chown ${OWNER_UID} "${CACHE_PATH}"
+chgrp ${OWNER_GID} "${CACHE_PATH}"
+chmod ${SPOOL_PERM} "${CACHE_PATH}"
+
+#now for the Perl system-level modules, CDS::Datastore et. al.
+#first find a good install location
+#this perl script is much easier than mucking around with awk etc!
+PMDIR=`perl ${SOURCE_DIR}/findpmpaths.pl`
+if [ "$?" -ne "0" ]; then
+	echo There seemed to be a problem finding a suitable path to install the PM files.
+	echo CDS will not run without these, check above trace for more information
+	exit 1
+fi
+
+echo Installing PM libraries to ${PMDIR}...
+
+for x in `find ${SOURCE_DIR}/CDS -iname \*.pm`; do
+	SRCPATH=`dirname "$x"`
+	#we need to strip our own source path out of the complete path to avoid attempting
+	#to install to silly locations
+	INSTPATH=${PMDIR}/`echo $SRCPATH | sed s#${SOURCE_DIR}/##`
+	#-D tells install to recursively create directories as needed
+	mkdir -p "${INSTPATH}"
+	chown ${OWNER_UID} "${INSTPATH}"
+	chgrp ${OWNER_GID} "${INSTPATH}"
+	install -bcpv -g ${OWNER_GID} -m ${BINARIES_PERM} -o ${OWNER_UID} "$x" ${INSTPATH}/`basename "$x"`
+	echo ${INSTPATH}/`basename $x` >> ${MANIFEST}
+done
+
+ln -s /usr/local/bin/cds_run.pl /usr/local/bin/cds_run
+ln -s /usr/local/bin/newsml_get.pl /usr/local/bin/newsml_get
+ln -s /usr/local/bin/cds_datareport.pl /usr/local/bin/datareport
+ln -s /usr/local/bin/cds_datastore.pl /usr/local/bin/cds_datastore
+ln -s /usr/local/bin/resolve_name.pl /usr/local/bin/resolve_name
+ln -s /usr/local/bin/ee_get.pl /usr/local/bin/ee_get
+
+echo The next step is to install any extra Perl libraries that are needed by CDS or the CDS methods.
+
+echo Checking you have the Perl modules you need \(logging to ${SOURCE_DIR}/checkperlmodules.log\)....
+perl ${SOURCE_DIR}/checkperlmodules.pl ${pkgargs} 2> ${SOURCE_DIR}/checkperlmodules.log
+
+echo
+cat << EOF
+CDS is now installed, now you need to set up some routes in ${ROUTES_PATH}.
+Consult the documentation for more details on this.
+At the moment, cds_run can only be run by root.  If you want to allow other users to run the CDS
+system, then you should add them to the CDS group, like this:
+$ usermod -aG cds {username}
+You will need to re-start any Terminal or login sessions for this to take effect.
+
+If you want to use a database to store logs, you should read the DBLOGGING.txt file in the installation directory, and copy the cds_backend.conf file from the etc/ subdirectory of the installation to /etc
+EOF
